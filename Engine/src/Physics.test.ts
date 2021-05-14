2c70a85320ba58ea2dc1b70c6e777c6626324196
styrheim <styrheim@olivant.fo>
2021-05-14 11:47:40 +0100

    Jest encountered an unexpected token

---
+++ Engine/src/Physics.test.ts
@@ -0,0 +1,9 @@
+import { Physics } from './Physics';
+// npm i --save-dev @types/jest
+// not enough, try:
+// npm install --save-dev jest
+// Run > Edit Configurations... > + > Jest
+test('a test' , () => {
+    let x = 1;
+    expect(x).toBe(0);
+})
---
+++ Engine/src/Physics.ts
@@ -0,0 +1,58 @@
+import * as  assert  from "assert";  // npm install @types/node --save-dev
+
+export class Physics{
+    Constructor(x: number, y: number, hVelocity: number, vVelocity: number, hAcceleration: number, vAcceleration: number){
+        // Not implemented yet
+        assert(false);
+    }
+
+    setPosition(x: number, y: number){
+        // Not implemented yet
+        assert(false);
+    }
+    getPosition(){
+        // Not implemented yet
+        assert(false);
+    }
+
+    setHorizontalPosition(x: number){
+        // Not implemented yet
+        assert(false);
+    }
+    getHorizontalPosition(){
+        // Not implemented yet
+        assert(false);
+    }
+
+    setVerticalPosition(y: number){
+        // Not implemented yet
+        assert(false);
+    }
+    getVerticalPosition(){
+        // Not implemented yet
+        assert(false);
+    }
+
+    setHorizontalVelocity(horizontalVelocity: number){
+        // Not implemented yet
+        assert(false);
+    }getHorizontalVelocity(){
+        // Not implemented yet
+        assert(false);
+    }
+
+    setVerticalVelocity(verticalVelocity: number){
+        // Not implemented yet
+        assert(false);
+    }
+
+    setVerticalAcceleration(vAccelleration: number){
+        // Not implemented yet
+        assert(false);
+    }
+
+    setHorizontalAcceleration(hAcceleration: number){
+        // Not implemented yet
+        assert(false);
+    }
+}
