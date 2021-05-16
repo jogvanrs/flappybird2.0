820a151017a36a31855704608cca5f5258eb65d7
styrheim <styrheim@olivant.fo>
2021-05-16 11:11:01 +0100

    Local LOOP_FREQUENCY available in test.

--- Engine/src/Physics.test.ts
+++ Engine/src/Physics.test.ts
@@ -2,8 +2,13 @@ import { Physics } from './Physics';
 // npm i --save-dev @types/jest
 // not enough, try:
 // npm install --save-dev jest
+// npm add @babel/preset-react
 // Run > Edit Configurations... > + > Jest
+
+const LOOP_FREQUENCY = 50; // To be replaced with a global constant?
+
 test('a test' , () => {
     let x = 1;
+    let Y = LOOP_FREQUENCY;
     expect(x).toBe(0);
 })
