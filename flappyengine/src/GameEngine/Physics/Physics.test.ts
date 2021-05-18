473a663e16426b42ba6701bd9a05adf1f400c787
styrheim <styrheim@olivant.fo>
2021-05-18 23:11:09 +0100

    setters tested. Failed: not implemented.

--- flappyengine/src/GameEngine/Physics/Physics.test.ts
+++ flappyengine/src/GameEngine/Physics/Physics.test.ts
@@ -10,6 +10,7 @@ const LOOP_FREQUENCY = 50; // To be replaced with a global constant?
 
 test('testing getters and setters' , () => {
     let physics = new Physics(1, 2, 3, 4, 5, 6);
+
     expect(physics.getHorizontalPosition()).toBe(1);
     expect(physics.getVerticalPosition()).toBe(2);
     expect(physics.getHorizontalVelocity()).toBe(3);
@@ -17,5 +18,24 @@ test('testing getters and setters' , () => {
     expect(physics.getHorizontalAcceleration()).toBe(5);
     expect(physics.getVerticalAcceleration()).toBe(6);
     expect(physics.getVerticalAcceleration()).toBeCloseTo(6) ;
+
+    physics.setPosition(101,102);
+    expect(physics.getHorizontalPosition()).toBe(101);
+    expect(physics.getVerticalPosition()).toBe(102);
+
+    physics.setHorizontalPosition(11);
+    physics.setVerticalPosition(12);
+    physics.setHorizontalVelocity(13);
+    physics.setVerticalVelocity(14);
+    physics.setHorizontalAcceleration(15);
+    physics.setVerticalAcceleration(16);
+
+    expect(physics.getHorizontalPosition()).toBe(11);
+    expect(physics.getVerticalPosition()).toBe(12);
+    expect(physics.getHorizontalVelocity()).toBe(13);
+    expect(physics.getVerticalVelocity()).toBe(14)
+    expect(physics.getHorizontalAcceleration()).toBe(15);
+    expect(physics.getVerticalAcceleration()).toBe(16);
+
 
 })
