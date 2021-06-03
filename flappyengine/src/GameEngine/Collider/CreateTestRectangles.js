2f58d4263c63f503c2a027b7b968c8babb2b3d1d
styrheim <styrheim@olivant.fo>
2021-06-03 12:07:33 +0100

    Compiles. BUt recctangle looks like a line...

--- flappyengine/src/GameEngine/Collider/CreateTestRectangles.js
+++ flappyengine/src/GameEngine/Collider/CreateTestRectangles.js
@@ -5,11 +5,14 @@ Using Javascript, because the .ts file extension for Typescript got the server c
 function createTestRectangle(id, top, left, height, width){
     const R = document.createElement("div");
     R.setAttribute("position","absolute");
+    R.setAttribute("top",top);
+    R.setAttribute("left", left);
+    R.setAttribute("height", height);
+    R.setAttribute("width",width);
+    return R;
 }
 
 function CreateTestRectangles(root){
-    createTestRectangle("R1",10,10,10,10);
-    const R1 = document.createElement("div");
-    R1.setAttribute("position","absolute");
+    const R1 = createTestRectangle("R1",10,10,10,10);
     root.appendChild(R1);
 }
