c513b17e019d2b302d8e9d26c778f88e1ed85d2a
styrheim <styrheim@olivant.fo>
2021-06-03 15:37:53 +0100

    Comment about nok including red borders in the generated DOMRects

--- flappyengine/src/GameEngine/Collider/CreateTestRectangles.js
+++ flappyengine/src/GameEngine/Collider/CreateTestRectangles.js
@@ -15,10 +15,24 @@ function createTestRectangle(id, top, left, height, width){
 }
 
 function CreateTestRectangles(root){
-    const R1 = createTestRectangle("R1",40,40,40,40);
-    const R2 = createTestRectangle("R2",40,120,40,40);
-    const R3 = createTestRectangle("R3",40,200,40,40);
+    const R1  = createTestRectangle("R1",40,40,40,40);
+    const R2  = createTestRectangle("R2",40,120,40,40);
+    const R3  = createTestRectangle("R3",40,200,40,40);
+    const R4  = createTestRectangle("R4",120,40,40,40);
+    const R5  = createTestRectangle("R5",120,120,40,40);
+    const R6  = createTestRectangle("R6",120,200,40,40);
+    const R7  = createTestRectangle("R7",200,40,40,40);
+    const R8  = createTestRectangle("R8",200,120,40,40);
+    const R9  = createTestRectangle("R9",200,200,40,40);
+    const R10 = createTestRectangle("R10",160,240,40,40);
     root.appendChild(R1);
     root.appendChild(R2);
     root.appendChild(R3);
+    root.appendChild(R4);
+    root.appendChild(R5);
+    root.appendChild(R6);
+    root.appendChild(R7);
+    root.appendChild(R8);
+    root.appendChild(R9);
+    root.appendChild(R10);
 }
