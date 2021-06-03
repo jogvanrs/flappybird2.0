58dc7a911064a2754224d7db25b16a7b25ebc503
styrheim <styrheim@olivant.fo>
2021-06-03 16:53:44 +0100

    Collider.collidesWith() defined

--- flappyengine/src/GameEngine/Collider/Collider.test.tsx
+++ flappyengine/src/GameEngine/Collider/Collider.test.tsx
@@ -1,10 +1,15 @@
 import { Collider } from './Collider';
+import assert from "assert";
 /*
 Mostly taken from previous project:
 https://github.com/Slow4life/FlappyBirdReact/blob/main/src/components/collision.test.tsx
  */
 
-test('constructor from html ', () => {
+test('constructor from html and DOmrect ', () => {
     const r = document.createElement("div");
     const collider = new Collider(r);
+    assert(collider);
+    const domRect = r.getBoundingClientRect();
+    const collider2 = new Collider(domRect);
+    assert(collider2);
 })
--- flappyengine/src/GameEngine/Collider/Collider.ts
+++ flappyengine/src/GameEngine/Collider/Collider.ts
@@ -1,10 +1,10 @@
 import React from 'react';
 import { strict as assert } from 'assert'; // from https://tinyurl.com/393vux8e
 export class Collider{
-    left: number;
-    top:number;
-    width: number;
-    height: number;
+    private left: number;
+    private top:number;
+    private width: number;
+    private height: number;
     constructor(htmlElement: HTMLElement|DOMRect){
         let domRect: DOMRect;
         if (htmlElement instanceof HTMLElement){
@@ -16,7 +16,27 @@ export class Collider{
         this.left = domRect.left;
         this.top = domRect.top;
         this.width = domRect.width;
-        this.height = domRect.height
-;    };
+        this.height = domRect.height;
+    };
+
+    private mayCollideWith(otherCollider: Collider){
+        /*
+        Return true if the bounding rectangles of
+        this and otherCollider overlap.
+         */
+        assert(false);
+        return false;
+    }
+
+    public collidesWith(mayCollideWith: Collider) {
+    /*
+    May be more elaborate in subclasses.
+     */
+    return this.mayCollideWith(otherCollider);
+    }
+
+
+
+
 
 }
