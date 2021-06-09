a8944ab628080c77a8a16dff5250aca2ffbdaddb
jogvanrs <jogvanrs@gmail.com>
2021-06-09 15:32:39 +0100

    inital commit

---
+++ flappyengine/src/FlappyBird/game/pipesThird.tsx
@@ -0,0 +1,30 @@
+import pipeN from '../assets/src/pipeN.png'
+import pipeS from '../assets/src/pipeS.png'
+
+import './sprite.css';
+
+const PipesThird = (props:any) => {
+
+    let gap = 450;
+    let bottomY = Math.random() * 170;
+
+    return(
+            <div id='pipesBothThird'>
+    
+            <div id='pipeUpperThird' style={{
+            width: 60,
+            height: 200,
+            bottom: bottomY + gap // Bottom pipe y + gap
+            }}><img src={pipeN} alt="null"/></div>
+    
+            <div id='pipeLowerThird' style={{
+            width: 60,
+            height: 200,
+            bottom: bottomY
+            }}><img src={pipeS} alt="null"/></div>
+
+            </div>
+      )
+}
+
+export default PipesThird;
---
+++ flappyengine/src/FlappyBird/game/playerSprite.tsx
@@ -0,0 +1,12 @@
+import birdPng from '../assets/src/bluebird-downflap.png';
+
+import './sprite.css';
+
+const PlayerSprite = (props:any) => {
+
+  let bird = <div id='playerSprite'><img src={birdPng} alt='PlayerSprite'/></div>
+
+  return bird
+}
+
+export default PlayerSprite;
