bb8e6992b0398f865707c688da16057a33dc776c
gittarin <jonasj2194@gmail.com>
2021-06-10 18:07:31 +0100

    fixed pipe hitbox

--- flappyengine/src/FlappyBird/game/components/Flappybird.tsx
+++ flappyengine/src/FlappyBird/game/components/Flappybird.tsx
@@ -9,6 +9,7 @@ import { ObjectManager } from '../../../GameEngine/recourceManager/ObjectManager
 import { GameLoop } from '../../../GameEngine/GameLoop/GameLoop'
 import './sprite.css';
 import { Collider } from "../../../GameEngine/Collider/Collider";
+import { group } from "console";
 
 function Flappybird() {
     return(
@@ -16,6 +17,7 @@ function Flappybird() {
 
                 <PlayerSprite/>
                 <PipesFirst />
+                <Ground></Ground>
         </div>
     )
  
@@ -30,31 +32,40 @@ window.onload = function() {
 let test = document.getElementById('pipesBothFirst');
 test.style.left = 500 + 'px';
 
-let playerobject = new ObjectManager('playerSprite' , 2, 2, 2, 2);
-let pipeObject = new ObjectManager('pipesBothFirst', 0, -45, 0, 0);
-
+let playerobject = new ObjectManager('playerSprite' , 2, 2, 2, 1);
+let pipeObject = new ObjectManager('pipesBothFirst', 0, -150, 0, 0);
 gameLoop.platformStart(calledFunctions);
 
 
 function calledFunctions() {
-    
+
+let playerCollider = new Collider(document.getElementById('playerSprite')); 
+let pipe1Collider = new Collider(document.getElementById('pipeLowerFirst'));
+let groundCollider = new Collider(document.getElementById('ground'));
+let pipe1uppCollider = new Collider(document.getElementById('pipeUpperFirst'))
     //playerobject.startmoveX();
 
     playerobject.moveY();
     pipeObject.moveX();
 
-    let playerCollider = new Collider(document.getElementById('playerSprite')) 
-    let pipe1Collider = new Collider(document.getElementById('pipeLowerFirst'))
-
-    playerCollider.collidesWith(pipe1Collider)
-    console.log('collide ' +  playerCollider.collidesWith(pipe1Collider))
+    console.log('collide ' +  playerCollider.collidesWith(groundCollider))
+    console.log('collide ' +  groundCollider.collidesWith(playerCollider))
     console.log('collide2 ' +  pipe1Collider.collidesWith(playerCollider))
- 
-    if (pipe1Collider.collidesWith(playerCollider) ) {
 
-            gameLoop.platformStop();
+    function gameOver(){
+    if(playerCollider.collidesWith(pipe1Collider) || 
+    playerCollider.collidesWith(groundCollider) || 
+    playerCollider.collidesWith(pipe1uppCollider)){
+       return true;
+    
+    }
+   }    
+   if(gameOver()){
+        gameLoop.platformStop();
     }
 }
+
+
 
 }
 export default Flappybird;
--- flappyengine/src/FlappyBird/game/components/pipesSecond.tsx
+++ flappyengine/src/FlappyBird/game/components/pipesSecond.tsx
@@ -5,17 +5,22 @@ import './sprite.css';
 
 const PipesSecond = (props:any) => {
 
-    let gap = 450;
-    let bottomY = Math.random() * 170;
+      let gap = 350;
+      let bottomY = Math.random() * 150;
+  
 
     return(
             <div id='pipesBothSecond'>
     
             <div id='pipeUpperSecond' style={{
             width: 60,
+            overflow: 'hidden',
             height: 200,
             bottom: bottomY + gap // Bottom pipe y + gap
-            }}><img src={pipeN} alt="null"/></div>
+            }}><img src={pipeN} alt="null" style={{
+                  width: '100%',
+                  height: '100%'
+            }}/></div>
     
             <div id='pipeLowerSecond' style={{
             width: 60,
