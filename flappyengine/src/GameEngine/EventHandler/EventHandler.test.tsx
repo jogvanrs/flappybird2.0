e864a4cbc8eccfe25680bb3589001f47b241e23f
styrheim <styrheim@olivant.fo>
2021-06-11 14:35:10 +0100

    Found toBeUndefined()

--- flappyengine/src/GameEngine/EventHandler/EventHandler.test.tsx
+++ flappyengine/src/GameEngine/EventHandler/EventHandler.test.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import { render, screen, fireEvent} from '@testing-library/react';
 import {EventHandler} from './EventHandler'
 
+test("testing for undefined boolean", ()=>{
+    let x: boolean|undefined;
+    expect(x).toBeUndefined();
+    expect(x).toBeDefined();
+})
+
 test("reacts to space down when listening for space down", ()=>{
     const eventHandler = new EventHandler();
     let gotSpaceDown = false;
@@ -10,5 +16,5 @@ test("reacts to space down when listening for space down", ()=>{
     eventHandler.keyPressDown(" ", () =>{
         gotSpaceDown = true;
     })
-    expect(gotSpaceDown);
+    expect(gotSpaceDown)
 })
