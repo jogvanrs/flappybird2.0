3265f2343f3fa42f3812e43fd0d91dafe8519391
styrheim <styrheim@olivant.fo>
2021-06-12 10:24:55 +0100

    Comment on KeyboardEvent.key vs .code,

--- flappyengine/src/GameEngine/EventHandler/EventHandler.ts
+++ flappyengine/src/GameEngine/EventHandler/EventHandler.ts
@@ -8,6 +8,12 @@ export class EventHandler{
     The event types "keydown" and "keyup" implies keyboard events only
     A future development will be handling other events, like mouse clicks.
      */
+    /*
+    To allow for local keyboards, we should really have tested on
+    KeyboardEvent.key, not .code, to handle local keybards correctly.
+    This implies testing for 'Space Bar' rather than 'Space'
+    As of 2021-06-12, Jan Egil could not make that work.
+     */
 
     keyPressDown(keycode : string, method: any) {
 
